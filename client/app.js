d8f7b436e80fb185e4fd7e0f83ba17e84e4ce5ff
analyn pureza <analyn.c@hotmail.com>
2022-06-26 17:49:36 -0500

    display weather data in console.

--- client/app.js
+++ client/app.js
@@ -1,12 +1,25 @@
 
-
-async function getWeather() {
-  const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=d088141b907de3effa5c6e2b4d9b08bf")
-  const data = await response.json()
-  console.log(data)
+// Get weather information from API
+function getWeather(city) {
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=d088141b907de3effa5c6e2b4d9b08bf").then(function(response) {
+  return response.json()
+}).then((data) => this.displayWeather(data));
 }
 
-getWeather()
+// Get weather data
+function displayWeather(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const {temp, humidity } = data.main;
+  console.log(name, icon, description, temp, humidity);
+  document.querySelector(".current-city").innerText = name;
+  document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  document.querySelector(".temp").innerText = temp + " °F";
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".humidity").innerText = humidity;
+
+}
+
 
 /**const form = document.querySelector(".weather-display form");
  
