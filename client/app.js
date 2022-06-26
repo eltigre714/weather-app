c68d66006bd1f4f238cab4bd72f815d9d90d80c7
analyn pureza <analyn.c@hotmail.com>
2022-06-26 00:45:48 -0500

    Updated app.js, style.css, and weather.ap.html.

--- client/app.js
+++ client/app.js
@@ -1,13 +1,14 @@
 
 console.log("TEsting")
 
-const form = document.querySelector(".weather-display container");
+const searchLocation = document.querySelector(".weather-display container");
  
-form.addEventListener("search", e => {
+searchLocation.addEventListener("search", e => {
   e.preventDefault();
   const inputVal = input.value;
 });
 
 
-//const inputVal = input.value;
-//const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${d088141b907de3effa5c6e2b4d9b08bf}&units=metric`;
+const inputVal = input.value;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${APIKEY}&units=metric`;
+
