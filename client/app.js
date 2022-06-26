a3f29eb95d677b0d2159ef6b450edeb15f71c0fa
analyn pureza <analyn.c@hotmail.com>
2022-06-25 23:00:13 -0500

    Added icons, current time, adjusted styling.

--- client/app.js
+++ client/app.js
@@ -1,2 +1,13 @@
 
 console.log("TEsting")
+
+const form = document.querySelector(".weather-display container");
+ 
+form.addEventListener("search", e => {
+  e.preventDefault();
+  const inputVal = input.value;
+});
+
+
+//const inputVal = input.value;
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${d088141b907de3effa5c6e2b4d9b08bf}&units=metric`;
