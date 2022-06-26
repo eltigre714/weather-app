2187180ee3903b76f51097cba6f32c2950645a69
analyn pureza <analyn.c@hotmail.com>
2022-06-26 09:57:49 -0500

    Testing API to console log.

--- client/app.js
+++ client/app.js
@@ -1,14 +1,21 @@
 
-console.log("TEsting")
 
-const searchLocation = document.querySelector(".weather-display container");
+async function getWeather() {
+  const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=d088141b907de3effa5c6e2b4d9b08bf")
+  const data = await response.json()
+  console.log(data)
+}
+
+getWeather()
+
+/**const form = document.querySelector(".weather-display form");
  
-searchLocation.addEventListener("search", e => {
+form.addEventListener("search", e => {
   e.preventDefault();
   const inputVal = input.value;
 });
 
 
 const inputVal = input.value;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${APIKEY}&units=metric`;
-
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${APIKEY}&units=metric`; 
+**/
